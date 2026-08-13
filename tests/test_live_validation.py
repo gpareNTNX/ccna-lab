5ad4a299355be3cb6cb09d531e96ff9b822bb07d
gpareNTNX <90346396+gpareNTNX@users.noreply.github.com>
2026-08-12 20:53:35 -0400

    Test native console session recovery

--- tests/test_live_validation.py
+++ tests/test_live_validation.py
@@ -7,6 +7,11 @@ class FakeAPI:
     def __init__(self):
         self.nodes_calls = 0
         self.node_calls = 0
+        self.login_calls = 0
+
+    def login(self):
+        self.login_calls += 1
+        return {"status": "success"}
 
     def nodes(self, _lab):
         self.nodes_calls += 1
@@ -25,10 +30,7 @@ class FakeAPI:
         return {"data": {}}
 
 
-class DelayedAPI:
-    def __init__(self):
-        self.nodes_calls = 0
-
+class DelayedAPI(FakeAPI):
     def nodes(self, _lab):
         self.nodes_calls += 1
         url = "" if self.nodes_calls < 2 else "telnet://127.0.0.1:32770"
@@ -42,47 +44,74 @@ class DelayedAPI:
             }
         }
 
-    def node(self, _lab, _node_id):
-        return {"data": {}}
+
+class HTML5ThenNativeAPI(FakeAPI):
+    def nodes(self, _lab):
+        self.nodes_calls += 1
+        if self.login_calls:
+            url = "telnet://127.0.0.1:32771"
+        else:
+            url = (
+                "/html5/#/client/MzI3NjkAYwBteXNxbA=="
+                "?token=F1666351184206978A3B4C5A78E5DA6225CA557FDEDCC7199962BB1537961091"
+            )
+        return {
+            "data": {
+                "1": {
+                    "name": "R1-EDGE",
+                    "console": "telnet",
+                    "url": url,
+                }
+            }
+        }
 
 
 class LiveValidationConsoleTests(unittest.TestCase):
-    def test_port_from_native_telnet_url(self):
+    def test_native_endpoint_from_telnet_url(self):
         self.assertEqual(
-            LiveValidator._port_from_url("telnet://127.0.0.1:32769"),
-            32769,
+            LiveValidator._native_endpoint_from_url("telnet://127.0.0.1:32769"),
+            ("127.0.0.1", 32769),
         )
 
-    def test_port_from_html5_guacamole_url(self):
+    def test_html5_guacamole_url_is_not_treated_as_telnet_port(self):
         url = (
             "/html5/#/client/MzI3NjkAYwBteXNxbA=="
             "?token=F1666351184206978A3B4C5A78E5DA6225CA557FDEDCC7199962BB1537961091"
         )
-        self.assertEqual(LiveValidator._port_from_url(url), 32769)
+        self.assertIsNone(LiveValidator._native_endpoint_from_url(url))
 
-    def test_invalid_html5_url_returns_none(self):
-        self.assertIsNone(LiveValidator._port_from_url("/html5/#/client/not-base64!"))
-
-    def test_console_port_from_node_list_url(self):
+    def test_console_endpoint_from_node_list_url(self):
         api = FakeAPI()
         validator = LiveValidator(api, ssh=None)
         node_info = api.nodes("/lab.unl")["data"]["1"]
 
-        port = validator._console_port(
+        endpoint = validator._console_endpoint(
             "/lab.unl", 1, node_info=node_info, attempts=1, delay=0
         )
 
-        self.assertEqual(port, 32769)
+        self.assertEqual(endpoint, ("127.0.0.1", 32769))
         self.assertEqual(api.node_calls, 0)
 
-    def test_console_port_retries_until_dynamic_url_exists(self):
+    def test_console_endpoint_retries_until_dynamic_url_exists(self):
         api = DelayedAPI()
         validator = LiveValidator(api, ssh=None)
 
-        port = validator._console_port("/lab.unl", 1, attempts=2, delay=0)
+        endpoint = validator._console_endpoint("/lab.unl", 1, attempts=2, delay=0)
 
-        self.assertEqual(port, 32770)
+        self.assertEqual(endpoint, ("127.0.0.1", 32770))
         self.assertGreaterEqual(api.nodes_calls, 2)
+
+    def test_html5_url_triggers_native_relogin(self):
+        api = HTML5ThenNativeAPI()
+        validator = LiveValidator(api, ssh=None)
+        initial = api.nodes("/lab.unl")["data"]["1"]
+
+        endpoint = validator._console_endpoint(
+            "/lab.unl", 1, node_info=initial, attempts=2, delay=0
+        )
+
+        self.assertEqual(endpoint, ("127.0.0.1", 32771))
+        self.assertEqual(api.login_calls, 1)
 
 
 if __name__ == "__main__":
